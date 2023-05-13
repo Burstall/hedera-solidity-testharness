9279a990ed41ee1f6bbdc7010b0e7f4db6bdf1be
Burstall <richard@alphacenturi.com>
2023-05-13 11:55:36 +0100

    tidy up

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-	version: "0.8.18",
-	settings: {
-		optimizer: {
-			enabled: true,
-			runs: 200,
+	solidity: {
+		version: '0.8.18',
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
 		},
 	},
-  },
-  contractSizer: {
-	alphaSort: true,
-	runOnCompile: true,
-	disambiguatePaths: false,
-	strict: true,
-},
+	contractSizer: {
+		alphaSort: true,
+		runOnCompile: true,
+		disambiguatePaths: false,
+		strict: true,
+	},
 };
